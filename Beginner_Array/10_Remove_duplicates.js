15f79b1d0da372a3ad5b3aecb07529913e41cfc3
Sachin Kumar <sachin.codes01@gmail.com>
2026-02-18 22:08:52 +0530

    add the files

--- Beginner_Array/10_Remove_duplicates.js
+++ Beginner_Array/10_Remove_duplicates.js
@@ -1,2 +1,38 @@
 ////Remove duplicates from a sorted array.
+let array = [1, 1, 2, 2, 3, 4, 4]
+let newArray = []
+newArray.push(array[0])
+for(let i=1;i<array.length;i++){
+    if(array[i] !== array[i-1]){
+        newArray.push(array[i])
+    }
+}
+console.log(newArray)
+
+
+
+////Using Set
+////Set automatically removes duplicates.
+//// Spread operator converts it back to array.
+let array1 = [1, 1, 2, 2, 3, 4, 4];
+let unique = [...new Set(array1)];
+console.log(unique);
+
+
+
+////In-Place Method (logical) (Best for Interviews)
+let arr = [1, 1, 2, 2, 3, 4, 4];
+let j = 0;
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== arr[j]) {
+        j++;
+        arr[j] = arr[i];
+    }
+}
+console.log(arr.slice(0, j + 1));
+
+
+
+
+
 
--- Beginner_Array/9_Move_all_zeros_to_the_end.js
+++ Beginner_Array/9_Move_all_zeros_to_the_end.js
@@ -1,1 +1,41 @@
 ////Move all zeros to the end of the array.
+
+let arr = [0,56,23,0,77,32,0,68,43];
+let newArr = []
+let newArr0 = []
+for(let i=0;i<arr.length;i++){
+    if(arr[i] !== 0){
+        newArr.push(arr[i])
+    }
+    if(arr[i] === 0){
+        newArr0.push(arr[i])
+    }
+}
+let myArr = newArr.concat(newArr0)
+console.log(myArr)
+
+////or
+////swap logic is like a=3,b=2 ,then temp = a ,now asume a is empty then a = b 
+//// ,now asume b is empty b = temp ,now a=2,b=3
+
+let arr1 = [0,56,23,0,77,32,0,68,43];
+let position = 0
+for(let i=0;i<arr1.length;i++){
+    if(arr1[i] !== 0){
+        let temp = arr1[i]
+        arr1[i] = arr1[position]
+        arr1[position] = temp
+
+        position++
+    }
+}
+console.log(arr1)
+
+
+////Using Filter Concept (Functional Thinking)
+
+let array = [0,56,23,0,77,32,0,68,43];
+let nonZero = array.filter(e=>e!==0)
+let zeros = array.filter(e=>e===0)
+let newArary = nonZero.concat(zeros)
+console.log(newArary)
