5429b54b35263cd7ccef2fcd5c65ff45ed26812d
Sachin Kumar <sachin.codes01@gmail.com>
2026-02-20 09:40:10 +0530

    files added

--- Strings/6_Remove_spaces.js
+++ Strings/6_Remove_spaces.js
@@ -1,2 +1,76 @@
 //// Remove spaces from a string
 
+
+////using methods
+////Remove all spaces
+let str = 'Whats on your mind today'
+let arr = str.split(' ').join('')
+console.log(arr)
+
+
+////Remove only leading and trailing spaces (from the startand end)
+let str1 = '            Whats on your mind today    '
+let arr1 = str1.trim()
+console.log(arr1)
+
+
+////Remove only extra spaces between words
+let str3 = 'Whats on         your mind                 today'
+let arr3 = str3.replace(/\s+/g, " ");
+console.log(arr3)
+
+
+
+
+////or using logic
+////Remove all spaces using loop
+let string = 'want to remove spaces from a string';
+let newStr = ''
+
+for(let i=0;i<string.length;i++){
+    if(string[i] !== " "){
+        newStr += string[i]
+    }
+}
+console.log(newStr)
+
+
+////Remove only leading and trailing spaces (from the startand end)
+let string1 = '          want to remove spaces from a string     ';
+let start = 0
+let end = string1.length-1
+let newStr1 = ''
+for(let i=0;i<string1.length;i++){
+    if(string1[i] !== " "){
+        start = i
+        break
+    }
+}
+for(let i=string1.length-1;i>=0;i--){
+    if(string1[i] !== " "){
+        end = i
+        break
+    }
+}
+for(let i=start;i<=end;i++){
+    newStr1 += string1[i]
+}
+console.log(newStr1)
+
+
+////Remove only extra spaces between words
+let strings = "Hello    World   From   JS"
+let result = ""
+let spaceCount = 0
+for(let i = 0; i < str.length; i++){
+    if(strings[i] === " "){
+        spaceCount++
+    }else{
+        spaceCount = 0
+    }
+
+    if(spaceCount<=1){
+        result += strings[i]
+    }
+}
+console.log(result)
--- Strings/7_Remove_duplicate.js
+++ Strings/7_Remove_duplicate.js
@@ -1,1 +1,33 @@
 //// Remove duplicate characters from string
+////using methods
+let str1 = "programming";
+let result1 = "";
+
+for (let i = 0; i < str1.length; i++) {
+    if (!result1.includes(str1[i])) {
+        result1 += str1[i];
+    }
+}
+console.log(result1);
+
+
+////using logic
+let str = "banana";
+let result = "";
+
+for (let i = 0; i < str.length; i++) {
+    let found = false;
+
+    for (let j = 0; j < result.length; j++) {
+        if (str[i] === result[j]) {
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        result += str[i];
+    }
+}
+
+console.log(result);
